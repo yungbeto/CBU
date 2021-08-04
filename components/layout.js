c1e58a9f5dab714d635e367b4b3f66279e8eed79
Roby Saavedra <roby.saavedra@gmail.com>
2021-08-04 00:01:10 -0400

    more

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Pook'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Cool Breeze Unlimited'
 
 export default function Layout({ children, home }) {
     return (
