673f0ba6eadc4069f84717eafe554dfbd2334c01
Roby Saavedra <roby.saavedra@gmail.com>
2021-08-21 18:51:32 -0400

    added play button

--- components/Player/Player.js
+++ components/Player/Player.js
@@ -1,9 +1,17 @@
 import styles from "./Player.module.scss";
+import Play from "../../components/Icons/Play"
+import Pause from "../../components/Icons/Pause"
+import { useState } from "react";
 
-export default function Player() {
+export default function Player(props) {
+  const [isPlaying, setIsPlaying] = useState('');
+
+
   return (
     <section className={styles.player}>
-      <button>play</button>
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? <Pause /> : <Play />}
+      </button>
     </section>
   );
 }
