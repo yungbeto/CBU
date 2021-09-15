91e18fc6a56a5b88578054732e6082ca87dc9c2e
Roby Saavedra <roby.saavedra@gmail.com>
2021-09-14 21:35:03 -0400

    fuckin a
    it works

--- components/Player/Player.js
+++ components/Player/Player.js
@@ -1,24 +1,35 @@
+
 import styles from "./Player.module.scss";
 import Play from "../../components/Icons/Play"
 import Pause from "../../components/Icons/Pause"
 import { useSelector } from 'react-redux';
-import { useState } from "react";
-import ReactPlayer from "react-player";
+import Plyr from 'plyr-react';
+import 'plyr-react/dist/plyr.css'
 
 export default function Player(props) {
+
   const songPlaying = useSelector((state) => state.isPlaying.isCurrentlyPlaying);
   const songUrl = useSelector((state) => state.isPlaying.songUrl)
-  const [isPlaying, setIsPlaying] = useState('');
-
-
+  const plyrSource = {
+    type: 'audio',
+    options: [{
+      autoplay: true
+    }],
+    sources: [{
+      src: `${songUrl}`
+    }]
+  };
   return (
-    <ReactPlayer url={songUrl} playing="true"/>
-    // <section className={styles.player}>
-    //   <p>{songPlaying}
-    // {songUrl}</p>
-    //   <button onClick={() => setIsPlaying(!isPlaying)}>
-    //     {isPlaying ? <Pause /> : <Play />}
-    //   </button>
-    // </section>
+    <section className={styles.player}>
+      <p>{songPlaying}</p>
+      <p>{songUrl}</p>
+      <Plyr
+        source={plyrSource}
+        autoPlay='true'
+      />
+
+    </section>
   );
+
 }
+
