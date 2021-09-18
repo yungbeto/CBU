d232efdf5e77d54c7e5fcc65a8d92ebcb05adf30
Roby Saavedra <roby.saavedra@gmail.com>
2021-09-18 00:14:21 -0400

    updated drill header

--- components/DrillHeader/DrillHeader.js
+++ components/DrillHeader/DrillHeader.js
@@ -3,13 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function DrillHeader() {
-
   return (
     <header className={styles.header}>
-        <div className={styles.top}>
-
-        🌴 Cool Breeze Unlimited
-        </div>
+      <div className={styles.top}>
+        <Link href="/">🌴 Cool Breeze Unlimited</Link>
+      </div>
       <Link href="/">
         <p className={styles.heading}>← Back</p>
       </Link>
