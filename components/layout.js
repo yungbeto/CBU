3d655cb46767082b18a3e3546421a94b9455b7d5
Roby Saavedra <roby.saavedra@gmail.com>
2023-11-13 18:48:28 -0500

    card type

--- components/layout.js
+++ components/layout.js
@@ -1,27 +1,31 @@
-import Head from "next/head";
-import Header from "./Header";
-import DrillHeader from "./DrillHeader";
-import styles from "./layout.module.scss";
-import { useSelector } from "react-redux";
+import Head from 'next/head';
+import Header from './Header';
+import DrillHeader from './DrillHeader';
+import styles from './layout.module.scss';
+import { useSelector } from 'react-redux';
 
-export const siteTitle = "🎷 Cool Breeze Unlimited";
+export const siteTitle = '🎷 Cool Breeze Unlimited';
 
 export default function Layout({ children, home }) {
-  const songPlaying = useSelector((state) => state.isPlaying.isCurrentlyPlaying);
+  const songPlaying = useSelector(
+    (state) => state.isPlaying.isCurrentlyPlaying
+  );
   return (
-    <div className={!songPlaying ? `${styles.container}` : `${styles.activeContainer}`}>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Cool Breeze Unlimited is a mix series by Roby Saavedra that fosters positive beach energy."
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        {home ? 
-        <Header /> : <DrillHeader />
+    <div
+      className={
+        !songPlaying ? `${styles.container}` : `${styles.activeContainer}`
       }
+    >
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Cool Breeze Unlimited is a mix series supplying positive beach energy.'
+        />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary' />
+      </Head>
+      {home ? <Header /> : <DrillHeader />}
       <section className={styles.inner}>
         <main>{children}</main>
       </section>
